76499f082f450162d0b01cd7aeda5901232ddc8b
SirriRyisa <sirriryisa@gmail.com>
2022-10-24 09:51:15 +0100

    my first pythin commit

---
+++ numbers.py
@@ -0,0 +1,9 @@
+for numbers in range (1, 30):
+ if(numbers %2) == 0:
+  print(numbers)
+  
+numberss = [20, 31, 47, 53, 10]
+ 
+only_odd = [num for num in numberss if num % 2 == 1]
+ 
+print(only_odd)
